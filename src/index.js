812c1948d458b4502448b220b9ad371b7511943a
SerhiiKutovyi <wagenrex@gmail.com>
2022-12-27 18:59:11 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const axios = require('axios');
+
+const PixabeyFetch = {
+  
+  key: "32386885-8dbf1bc36075d10a6eaf5580b",
+  q: formRef,
+  image_type:
+  orientation:
+  safesearch:
+};
+
+const API_KEY = '32386885-8dbf1bc36075d10a6eaf5580b';
+const BASE_URL = 'https://pixabay.com/api/';
+const URL = `${BASE_URL}`;
+
+
+
+// const formRef = document.querySelector('.search-form');
+
+// formRef.addEventListener('submit', onClickSearch);
+
+// function onClickSearch(eve) {
+//   eve.preventDefault();
+//   console.log(eve);
+//   console.log(eve.target.elements[0].value);
+// }
