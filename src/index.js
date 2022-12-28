2f34af7f13e41c4d8b9e18aa51206501f5ce6b26
SerhiiKutovyi <wagenrex@gmail.com>
2022-12-28 17:03:40 +0200

    dfg

--- src/index.js
+++ src/index.js
@@ -1,28 +1,64 @@
+import './CSS/index.css';
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const axios = require('axios');
-
-const PixabeyFetch = {
-  
-  key: "32386885-8dbf1bc36075d10a6eaf5580b",
-  q: formRef,
-  image_type:
-  orientation:
-  safesearch:
-};
+const formRef = document.querySelector('.search-form');
+const galleryRef = document.querySelector('.gallery');
 
 const API_KEY = '32386885-8dbf1bc36075d10a6eaf5580b';
 const BASE_URL = 'https://pixabay.com/api/';
-const URL = `${BASE_URL}`;
 
+formRef.addEventListener('submit', onClickSearch);
 
+function onClickSearch(eve) {
+  eve.preventDefault();
 
-// const formRef = document.querySelector('.search-form');
+  const form = eve.target;
+  const input = form.elements.searchQuery.value;
 
-// formRef.addEventListener('submit', onClickSearch);
+  fetchSearch(input)
+    .then(users => renderSearch(users))
+    .catch(error => {
+      console.log(error);
+    });
+}
 
-// function onClickSearch(eve) {
-//   eve.preventDefault();
-//   console.log(eve);
-//   console.log(eve.target.elements[0].value);
-// }
+function fetchSearch(input) {
+  return fetch(
+    `${BASE_URL}?key=${API_KEY}&q=${input}&image_type=photo&orientation=horizontal&safesearch=true `
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(resold => {
+      return resold;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+function renderSearch({ hits }) {
+  const markup = hits
+    .map(ele => {
+      return `<div class="photo-card">
+    <img src="${ele.webformatURL}" alt="${ele.tags}" width="${ele.webformatWidth}" height="${ele.webformatHeight}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b><span>${ele.likes}</span>
+      </p>
+      <p class="info-item">
+        <b>Views:</b><span>${ele.views}</span>
+      </p>
+      <p class="info-item">
+        <b>Comments:</b><span>${ele.comments}</span>
+      </p>
+      <p class="info-item">
+        <b>Downloads:</b><span>${ele.downloads}</span>
+      </p>
+    </div>
+  </div>`;
+    })
+    .join('');
+  galleryRef.insertAdjacentHTML('beforeend', markup);
+}
